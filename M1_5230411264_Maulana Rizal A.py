842058fa5a43bfb5f5314dbb6264164184188ec9
rizallal <maulanarizalalvani@gmail.com>
2024-09-25 19:39:15 +0700

    Add files via upload

---
+++ M1_5230411264_Maulana Rizal A.py
@@ -0,0 +1,10 @@
+def mulai():
+    p = input()
+    l = input()
+# Main
+print("masukkan alas = ")
+alas = int(input())
+print("masukan tinggi = ")
+tinggi = int(input())
+luas = float(alas * tinggi) / 2
+print("luas segitiga = " + str(luas))
